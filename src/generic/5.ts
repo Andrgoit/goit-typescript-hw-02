3515adf788fee358e4cce9683426757d0086da6c
Andrgoit <stakgrom@gmail.com>
2024-10-14 23:56:57 +0300

    add generic

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,14 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<T, K> {
+  key: T;
+  value: K;
 }
+
+const example: KeyValuePair<string, number> = {
+  key: "age",
+  value: 22,
+};
 
 export {};
